a1ba07560f0920cd07aa3960f99db8c5ee0dcde0
Jonathan Neely <jneely@gmail.com>
2022-05-05 12:10:39 -0700

    Moved instructions to README file instead of commented in the JS

--- js/main.js
+++ js/main.js
@@ -1,33 +1,6 @@
 /**
- * Random Quote Page!
- *
- * This project should take under two hours. We obviously have no way to time you, but would prefer if
- *  you gave yourself a stopping time two hours after you start and just send over whatever you have
- *
- * If you're not done in that window, that's okay. Feel free to explain where you were, what got
- *  you stuck, and where your next steps would be after your time ran out.
- *
- * For this project, we want you to use an API to generate random quotes. While the quote is being
- *  retreived, we want to show a diagonal screen wipe with a new, random color each time. See the
- *  example here: https://youtu.be/uBtGr9XcUQc
- *
- * This is just an example and it does not have to look exactly like that. In fact, the quote section
- *  is pretty ugly and could be improved if you have time and an eye for design. If not, that's okay too.
- *
- * You can find the random quote API endpoint here:
+ * See the README file for more information. API endpoint here for convenience:
  * http://safetybelt.pythonanywhere.com/quotes/random
- *
- * Note that this API intentionally kind of sucks. It's pretty quick, but it does have an issue where
- *  it fails about 10% of the time. We can't fix the API, so we'll need to figure out a decent way
- *  to handle that.
- *
- * The HTML, CSS, and JavaScript are purely boilerplate to get you started. Feel free to change as much
- *  or as little of it as you would like.
- *
- * We also have another example where the user doesn't even have to click- the page automatically generates
- *  a new quote after a set amount of time (can be any length). If you're done with the above, feel free
- *  to tackle this part. It's not required and should be done only if you have time.
- *  Example: https://youtu.be/LFl9gZHSEQ0
  */
 
 
