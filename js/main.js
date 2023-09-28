9e4548c616cc2458afa57d1a480c2214cd6d432f
Rain <pyc0422@gmail.com>
2023-09-28 12:20:00 -0500

    meet the bonus requirement

--- js/main.js
+++ js/main.js
@@ -6,15 +6,27 @@
 const url = "http://safetybelt.pythonanywhere.com/quotes/random"
 
 function generate() {
+    const container = document.body;
     const elem = document.getElementById('quote-text');
     const author = document.getElementById('quote-author');
-    elem.innerHTML = "Quote goes here!";
+    const currentColor = getComputedStyle(document.documentElement).getPropertyValue('--bg-color-2')
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const position = container.style.backgroundPosition;
     fetch(url)
         .then(res => res.json())
         .then(data => {
             if (data.quote && data.author){
                 elem.textContent = data.quote;
                 author.textContent = "- " + data.author;
+                if (position === 'right top') {
+                    document.documentElement.style.setProperty('--bg-color-2', currentColor);
+                    document.documentElement.style.setProperty('--bg-color-1', randomColor);
+                    container.style.backgroundPosition = 'left bottom'
+                } else {
+                    document.documentElement.style.setProperty('--bg-color-1', currentColor);
+                    document.documentElement.style.setProperty('--bg-color-2', randomColor);
+                    container.style.backgroundPosition = 'right top'
+                }
             } else {
                 generate()
             }
@@ -29,23 +41,14 @@ function generate() {
     const container = document.body;
     generate()
     if (button) {
+        generate()
         button.addEventListener('click', (e) => {
             e.preventDefault();
-            const currentColor = getComputedStyle(document.documentElement).getPropertyValue('--bg-color-2')
-            const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-            const position = container.style.backgroundPosition;
-            if (position === 'right top') {
-                document.documentElement.style.setProperty('--bg-color-2', currentColor);
-                document.documentElement.style.setProperty('--bg-color-1', randomColor);
-                container.style.backgroundPosition = 'left bottom'
-            } else {
-                document.documentElement.style.setProperty('--bg-color-1', currentColor);
-                document.documentElement.style.setProperty('--bg-color-2', randomColor);
-                container.style.backgroundPosition = 'right top'
-            }
-
-            generate();
         });
+    } else {
+        setInterval(() => {
+            generate()
+        }, 3000)
     }
 })();
 
